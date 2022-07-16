c31171c9554dfc5369151132f1e4e4e165963fa4
Luigi Quattrociocchi <luigi@quattrociocchi.net>
2022-07-15 22:56:03 -0400

    Line drawing function

--- src/kernel/screen.c
+++ src/kernel/screen.c
@@ -36,7 +36,7 @@ void screen_draw_bitmap(const u16* buff, u16 x, u16 y, u16 width, u16 height) {
                 u8 col = buff[i * width + j] & 0xFF;
                 u16 sx = x + j;
                 u16 sy = y + i;
-                if (0 <= sx && sx < SCREEN_WIDTH && 0 <= sy && sy < SCREEN_HEIGHT)
+                if (sx < SCREEN_WIDTH && sy < SCREEN_HEIGHT)
                     screen_set_px(sx, sy, col);
             }
         }
@@ -51,9 +51,97 @@ void screen_draw_bitmap_mono(const u8* buff, u16 x, u16 y, u16 width, u16 height
                 if (b & (1U << (7 - s))) {
                     u16 sx = x + j * 8 + s;
                     u16 sy = y + i;
-                    if (0 <= sx && sx < SCREEN_WIDTH && 0 <= sy && sy < SCREEN_HEIGHT)
+                    if (sx < SCREEN_WIDTH && sy < SCREEN_HEIGHT)
                         screen_set_px(sx, sy, 0xFF);
                 }
+            }
+        }
+    }
+}
+
+void screen_draw_line(i32 x1, i32 y1, i32 x2, i32 y2, u8 col) {
+    i32 dx = x2 - x1;
+    i32 dy = y2 - y1;
+
+    // simple lines
+    if (dx == 0) {
+        if (y2 < y1) {
+            i32 t = y2;
+            y2 = y1;
+            y1 = t;
+        }
+        for (i32 y = y1; y <= y2; ++y)
+            if (0 <= x1 && x1 < SCREEN_WIDTH && 0 <= y && y < SCREEN_HEIGHT)
+                screen_set_px(x1, y, col);
+    }
+    else if (dy == 0) {
+        if (x2 < x1) {
+            i32 t = x2;
+            x2 = x1;
+            x1 = t;
+        }
+        for (i32 x = x1; x <= x2; ++x)
+            if (0 <= x && x < SCREEN_WIDTH && 0 <= y1 && y1 < SCREEN_HEIGHT)
+                screen_set_px(x, y1, col);
+    }
+    else {
+        u8 dxPos = dx > 0;
+        u8 dyPos = dy > 0;
+        i32 dx1 = dxPos ? dx : -dx;
+        i32 dy1 = dyPos ? dy : -dy;
+        i32 px = 2 * dy1 - dx1;
+        i32 py = 2 * dx1 - dy1;
+        i32 x, y, xe, ye;
+        if (dy1 <= dx1) {
+            if (dx >= 0) {
+                x = x1;
+                y = y1;
+                xe = x2;
+            }
+            else {
+                x = x2;
+                y = y2;
+                xe = x1;
+            }
+            if (0 <= x && x < SCREEN_WIDTH && 0 <= y && y < SCREEN_HEIGHT)
+                screen_set_px(x, y, col);
+            for (i32 i = 0; x < xe; ++i) {
+                ++x;
+                if (px < 0) {
+                    px += 2 * dy1;
+                }
+                else {
+                    y += dxPos == dyPos ? 1 : -1;
+                    px += 2 * (dy1 - dx1);
+                }
+                if (0 <= x && x < SCREEN_WIDTH && 0 <= y && y < SCREEN_HEIGHT)
+                    screen_set_px(x, y, col);
+            }
+        }
+        else {
+            if (dy >= 0) {
+                x = x1;
+                y = y1;
+                ye = y2;
+            }
+            else {
+                x = x2;
+                y = y2;
+                ye = y1;
+            }
+            if (0 <= x && x < SCREEN_WIDTH && 0 <= y && y < SCREEN_HEIGHT)
+                screen_set_px(x, y, col);
+            for (i32 i = 0; y < ye; ++i) {
+                ++y;
+                if (py <= 0) {
+                    py += 2 * dx1;
+                }
+                else {
+                    x += dxPos == dyPos ? 1 : -1;
+                    py += 2 * (dx1 - dy1);
+                }
+                if (0 <= x && x < SCREEN_WIDTH && 0 <= y && y < SCREEN_HEIGHT)
+                    screen_set_px(x, y, col);
             }
         }
     }
--- src/kernel/screen.h
+++ src/kernel/screen.h
@@ -13,11 +13,11 @@ extern u8 g_sbuff_front;
 
 void screen_draw_bitmap(const u16* buff, u16 x, u16 y, u16 width, u16 height);
 void screen_draw_bitmap_mono(const u8* buff, u16 x, u16 y, u16 width, u16 height);
+void screen_draw_line(i32 x1, i32 y1, i32 x2, i32 y2, u8 col);
 void screen_fill(u8 col);
 void screen_swap_buffers(void);
 void screen_init(void);
 
 #define screen_set_px(x, y, col) g_sbuff[g_sbuff_front][(y) * (SCREEN_WIDTH) + (x)] = (col)
-
 
 #endif // SCREEN_H
